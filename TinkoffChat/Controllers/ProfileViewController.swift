ede71fa8deadddc1b974fbc50c7ad103f40f937c
s.zorin <s.zorin@tinkoff.ru>
2020-02-21 15:39:38 +0300

    Implement navigation.

---
+++ TinkoffChat/Controllers/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  TinkoffChat
+//
+//  Created by s.zorin on 20.02.2020.
+//  Copyright © 2020 Tinkoff Bank. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    // MARK: - Actions
+    
+    @IBAction func navigateBack(_ sender: UIButton) {
+        navigationController?.navigateBack()
+    }
+}

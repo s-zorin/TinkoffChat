d59e39ec610d588de8da419885742dc591150fe9
s.zorin <s.zorin@tinkoff.ru>
2020-02-22 22:08:17 +0300

    Implement image picker.

--- TinkoffChat/Controllers/ProfileViewController.swift
+++ TinkoffChat/Controllers/ProfileViewController.swift
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // MARK: - Outlets
     
     @IBOutlet private var profileImage: UIImageView!
-    @IBOutlet private var selectImageButton: UIButton!
+    @IBOutlet private var pickImageButton: UIButton!
     
     // MARK: - Actions
     
@@ -21,11 +21,44 @@ class ProfileViewController: UIViewController {
         navigationController?.navigateBack()
     }
     
+    @IBAction private func pickImage(_ sender: UIButton) {
+        print("Pick profile image.")
+        let controller = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let photoLibraryAction = UIAlertAction(title: "Установить из галереи",
+                                               style: .default,
+                                               handler: {[weak self] (_) in self?.pickImage(sourceType: .photoLibrary)})
+        let cameraAction = UIAlertAction(title: "Сделать фото",
+                                         style: .default,
+                                         handler: {[weak self] (_) in self?.pickImage(sourceType: .camera)})
+        controller.addAction(photoLibraryAction)
+        controller.addAction(cameraAction)
+        present(controller, animated: true)
+    }
+    
     // MARK: - Overrides
     
     override func viewDidLoad() {
         super.viewDidLoad()
         profileImage.layer.cornerRadius = 48
-        selectImageButton.layer.cornerRadius = 48
+        pickImageButton.layer.cornerRadius = 48
+    }
+    
+    // MARK: - Implementation of UIImagePickerControllerDelegate
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        profileImage.image = info[.originalImage] as? UIImage
+    }
+    
+    // MARK: - Private Methods
+    
+    private func pickImage(sourceType: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = self
+            imagePickerController.sourceType = sourceType
+            imagePickerController.allowsEditing = false
+            present(imagePickerController, animated: true)
+        }
     }
 }
